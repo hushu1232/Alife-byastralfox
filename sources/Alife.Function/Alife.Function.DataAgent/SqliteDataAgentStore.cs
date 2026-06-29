91218190c0629dfdd6cf9e854bed15a1cd77c6d4
hushu <3045846738@qq.com>
2026-06-29 16:48:10 +0800

    Add DataAgent store contract and SQLite provider

---
+++ sources/Alife.Function/Alife.Function.DataAgent/SqliteDataAgentStore.cs
@@ -0,0 +1,58 @@
+namespace Alife.Function.DataAgent;
+
+public sealed class SqliteDataAgentStore(string databasePath) : IDataAgentStore
+{
+    public string ProviderName => "sqlite";
+
+    public void Initialize()
+    {
+        DataAgentSchemaInitializer.Initialize(databasePath);
+    }
+
+    public void ImportFixtures()
+    {
+        DataAgentFixtureImporter.Import(databasePath);
+    }
+
+    public DataAgentQueryResult Query(DataAgentCompiledSql compiledSql)
+    {
+        return new DataAgentQueryExecutor(databasePath).Execute(compiledSql);
+    }
+
+    public void RecordAccepted(DataAgentAcceptedAuditInput input)
+    {
+        new DataAgentAuditLog(databasePath).RecordAccepted(
+            input.Question,
+            input.Dataset,
+            input.QueryPlanJson,
+            input.GeneratedSql,
+            input.RowCount,
+            input.Elapsed);
+    }
+
+    public void RecordRejected(DataAgentRejectedAuditInput input)
+    {
+        new DataAgentAuditLog(databasePath).RecordRejected(
+            input.Question,
+            input.Dataset,
+            input.QueryPlanJson,
+            input.GeneratedSql,
+            input.RejectedReason,
+            input.Elapsed);
+    }
+
+    public IReadOnlyList<DataAgentAuditRecord> ReadQueryAudit()
+    {
+        return new DataAgentAuditLog(databasePath).ReadAll();
+    }
+
+    public void RecordToolBrokerAudit(DataAgentToolBrokerAuditRecord record)
+    {
+        new DataAgentToolBrokerAuditLog(databasePath).Record(record);
+    }
+
+    public IReadOnlyList<DataAgentToolBrokerAuditRecord> ReadToolBrokerAudit()
+    {
+        return new DataAgentToolBrokerAuditLog(databasePath).ReadAll();
+    }
+}
