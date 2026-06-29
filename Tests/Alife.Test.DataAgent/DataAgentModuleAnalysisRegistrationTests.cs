2ca19ba6193d4f67705d2574ba4dec6386465cc6
hushu <3045846738@qq.com>
2026-06-29 14:56:34 +0800

    Harden DataAgent module capability registration tests

--- Tests/Alife.Test.DataAgent/DataAgentModuleAnalysisRegistrationTests.cs
+++ Tests/Alife.Test.DataAgent/DataAgentModuleAnalysisRegistrationTests.cs
@@ -1,22 +1,57 @@
+using Alife.Framework;
+using Alife.Function.DataAgent;
+using Alife.Function.FunctionCaller;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+
 namespace Alife.Test.DataAgent;
 
 [TestFixture]
 public sealed class DataAgentModuleAnalysisRegistrationTests
 {
     [Test]
-    public void AwakeRegistersQueryAndAnalysisHandlers()
+    public async Task AwakeRegistersCapabilityProviderHandlersIntoFunctionCaller()
     {
-        string source = ReadModuleSource();
+        XmlFunctionCaller functionCaller = new(NullLogger<XmlFunctionCaller>.Instance);
+        DataAgentModuleService service = new(functionCaller);
+
+        await service.AwakeAsync(new AwakeContext
+        {
+            Character = new Character(),
+            Services = new ServiceCollection().BuildServiceProvider(),
+            KernelBuilder = Kernel.CreateBuilder(),
+            ContextBuilder = new ChatHistoryAgentThread()
+        });
 
         Assert.Multiple(() =>
         {
-            Assert.That(source, Does.Contain("new DataAgentToolHandler(service, Poke)"));
-            Assert.That(source, Does.Contain("RegisterHandlerWithoutDocument(xmlHandler)"));
-            Assert.That(source, Does.Contain("new InMemoryDataAgentAnalysisSessionStore()"));
-            Assert.That(source, Does.Contain("new DataAgentAnalysisService(service, analysisSessionStore)"));
-            Assert.That(source, Does.Contain("new DataAgentAnalysisToolHandler(analysisService, PublishAnalysisContext)"));
-            Assert.That(source, Does.Contain("RegisterHandlerWithoutDocument(analysisXmlHandler)"));
+            Assert.That(functionCaller.CanHandleFunction("dataagent_query"), Is.True);
+            Assert.That(functionCaller.CanHandleFunction("dataagent_analysis_start"), Is.True);
+            Assert.That(functionCaller.CanHandleFunction("dataagent_analysis_continue"), Is.True);
+            Assert.That(functionCaller.CanHandleFunction("dataagent_analysis_summarize"), Is.True);
+            Assert.That(functionCaller.CanHandleFunction("dataagent_analysis_end"), Is.True);
+            Assert.That(service.RegisteredCapabilityProviderNames, Is.EqualTo(new[]
+            {
+                nameof(DataAgentQueryCapabilityProvider),
+                nameof(DataAgentAnalysisCapabilityProvider)
+            }));
+            Assert.That(service.RegisteredCapabilityToolNames, Is.EqualTo(new[]
+            {
+                "dataagent_query",
+                "dataagent_analysis_start",
+                "dataagent_analysis_continue",
+                "dataagent_analysis_summarize",
+                "dataagent_analysis_end"
+            }));
         });
+    }
+
+    [Test]
+    public void CapabilityRegistrarRejectsNullFunctionService()
+    {
+        Assert.Throws<ArgumentNullException>(() => new DataAgentCapabilityRegistrar(null!));
     }
 
     [Test]
@@ -31,6 +66,9 @@ public sealed class DataAgentModuleAnalysisRegistrationTests
             Assert.That(source, Does.Contain("Only use DataAgent XML tools when they appear in current [tool_route_context]"));
             Assert.That(source, Does.Not.Contain("{xmlHandler.FunctionDocument()}"));
             Assert.That(source, Does.Not.Contain("{analysisXmlHandler.FunctionDocument()}"));
+            Assert.That(source, Does.Not.Contain("<dataagent_query"));
+            Assert.That(source, Does.Not.Contain("<dataagent_analysis_start"));
+            Assert.That(source, Does.Not.Contain("<dataagent_analysis_continue"));
         });
     }
 
